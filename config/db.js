a54c256c243e12fd349bd7cc64db0a50f2a4b8b8
LAPTOP-JJ518A0L\minht <minhthanh5346@gmail.com>
2024-05-01 21:23:30 +0700

    asdasd

--- config/db.js
+++ config/db.js
@@ -9,4 +9,32 @@ const db = mysql.createConnection({
   port: process.env.DB_PORT, 
 });
 
+const createTablesQueries = [
+  `CREATE TABLE CUSTOMER (
+      customer_id INT PRIMARY KEY AUTO_INCREMENT,
+      name VARCHAR(100),
+      email VARCHAR(100),
+      password VARCHAR(100)
+  )`,
+  `CREATE TABLE CUSTOMER_DETAIL (
+      customer_id INT,
+      phone VARCHAR(20),
+      address VARCHAR(255),
+      FOREIGN KEY (customer_id) REFERENCES CUSTOMER(customer_id)
+  )`,
+];
+
+function createTables() {
+  createTablesQueries.forEach(query => {
+    db.query(query, (err, results) => {
+          if (err) {
+              console.error('Error creating table:', err);
+              return;
+          }
+          console.log('Table created successfully');
+      });
+  });
+}
+
+createTables() ;
 module.exports = db; // Export the promise-based pool
